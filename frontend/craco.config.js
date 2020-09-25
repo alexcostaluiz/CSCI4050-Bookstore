29ae1510c4de01f234ca018c9be86d78c80d292a
Alexander Luiz Costa <alexcostaluiz@outlook.com>
2020-09-25 05:52:24 -0400

    Add Monserrat font to global font-family var

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -9,6 +9,8 @@ module.exports = {
           '@primary-color': '#FF1053',
           '@border-radius-base': '8px',
           '@btn-text-shadow': 'none',
+          '@btn-font-weight': '700',
+          '@font-family': 'Montserrat, -apple-system, BlinkMacSystemFont, \'Segoe UI\', Roboto, \'Helvetica Neue\', Arial, \'Noto Sans\', sans-serif, \'Apple Color Emoji\', \'Segoe UI Emoji\', \'Segoe UI Symbol\', \'Noto Color Emoji\';'
         }
       }
     }
