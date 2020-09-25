9bc1c93ba762d111da973a0c5df93426bc1e52f2
Alexander Luiz Costa <alexcostaluiz@outlook.com>
2020-09-25 04:39:04 -0400

    Design Header component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,83 @@
-import './Header.css';
+import './Header.less';
 
-import React from 'react';
+import React, { useState } from 'react';
+
+import {
+  Row,
+  Col,
+  Layout,
+  Typography,
+  Menu,
+  Space,
+  AutoComplete,
+  Input,
+  Avatar,
+  Button
+} from 'antd';
+
+const { Header: AntHeader } = Layout;
+const { Title } = Typography;
+
+function DynamicAvatar(props) {
+  const { isSignedIn } = props;
+
+  if (isSignedIn) {
+    return <Button type='primary' size='large' shape='circle'>AC</Button>;
+  } else {
+    return <Button type='primary' size='large'>Sign In</Button>;
+  }
+}
 
 function Header(props) {
+  const [current, setCurrent] = useState('home');
+
+  const handleClick = e => {
+    setCurrent(e.key);
+  };
+  
   return (
-    <div className='header'>
-      <h4>Header</h4>
-    </div>
+    <AntHeader style={{background: 'white', borderBottom: '1px solid #ccc'}}>
+      <Row align='middle' justify='space-between'>
+        <Col>
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <Title 
+              level={2}
+              style={{ margin: '0px 38px 4px 0px', lineHeight: 1 }}>
+              Bookstore
+            </Title>
+            <Menu
+              onClick={handleClick}
+              selectedKeys={[current]}
+              mode='horizontal'
+              style={{borderBottom: 'none', background: 'none', lineHeight: '62px'}}>
+              <Menu.Item key='home'>
+                Home
+              </Menu.Item>
+              <Menu.Item key='browse'>
+                Browse
+              </Menu.Item>
+              <Menu.Item key='nav3'>
+                Nav3
+              </Menu.Item>
+              <Menu.Item key='nav4'>
+                Nav4
+              </Menu.Item>
+              <Menu.Item key='nav5'>
+                Nav5
+              </Menu.Item>
+            </Menu>
+          </div>
+        </Col>
+        <Col>
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <AutoComplete className='bookstore-search-dropdown'>
+              <Input.Search size='small' placeholder='Try: "Science Fiction"' />
+            </AutoComplete>
+            <DynamicAvatar isSignedIn={true} />
+          </div>
+        </Col>
+      </Row>
+    </AntHeader>
   );
 }
 
