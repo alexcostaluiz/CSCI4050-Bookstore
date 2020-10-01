3dd5af95ac63fc2e034e88aa63985cfee7fa346c
Alexander Luiz Costa <alexcostaluiz@outlook.com>
2020-09-30 23:18:23 -0400

    Create CheckoutPage component

---
+++ frontend/src/pages/CheckoutPage.js
@@ -0,0 +1,55 @@
+import './CheckoutPage.less';
+
+import React from 'react';
+
+import { Breadcrumb, Button, Col, Row, Typography } from 'antd';
+
+import CartItem from '../components/CartItem.js';
+import CartSummary from '../components/CartSummary.js';
+
+const { Paragraph, Title } = Typography;
+
+function CheckoutPage(props) {
+  const { cart } = props;
+  
+  return (
+    <Row justify='center'>
+      <Col span={24} className='bookstore-column'>
+        <Breadcrumb className='bookstore-breadcrumb'>
+          <Breadcrumb.Item href='/'>Home</Breadcrumb.Item>
+          <Breadcrumb.Item href='/cart'>Cart</Breadcrumb.Item>
+          <Breadcrumb.Item href='#'>Checkout</Breadcrumb.Item>
+        </Breadcrumb>
+        <div className='bookstore-page-section'>
+
+          <div className='bookstore-checkout-module-container'>
+            <div className='bookstore-checkout-module'>
+              <Title className='bookstore-checkout-module-title'>Shipping Address</Title>
+            </div>
+
+            <div className='bookstore-checkout-module'>
+              <Title className='bookstore-checkout-module-title'>Billing Information</Title>
+            </div>
+            
+            <div className='bookstore-cart-list'>
+              <Title className='bookstore-cart-list-title'>Cart</Title>
+              {cart.map((b, i) => (
+                <CartItem key={i} {...b} />
+              ))}
+            </div>
+          </div>
+          
+          <CartSummary
+            cart={cart}
+            action={
+              <Button type='primary' size='large' block>PLACE ORDER</Button>
+            }
+          />
+        </div>
+      </Col>
+    </Row>
+
+  );
+}
+
+export default CheckoutPage;
