54f4c3e5b54897b89827dd752805118b2e7bfcd3
Alexander Luiz Costa <alexcostaluiz@outlook.com>
2020-09-30 23:18:03 -0400

    Move common page classes to global.less

--- frontend/src/pages/CartPage.js
+++ frontend/src/pages/CartPage.js
@@ -12,33 +12,17 @@ import Slider from '../components/Slider.js';
 
 const { Title } = Typography;
 
-const book = {
-  author: 'Barack Obama',
-  edition: null,
-  image: 'https://kottke.org/plus/misc/images/obama-promised-land-book.jpg',
-  isbn: '978-1-524-76316-9',
-  numRatings: 4132,
-  pages: 768,
-  price: 34.99,
-  publicationDate: 1605589200,
-  publisher: 'Crown Publishing Group',
-  rating: 4.1,
-  tags: ['Nonfiction', 'Biography', 'Autobiography', 'Bestseller'],
-  title: 'A Promised Land',
-};
-const fcart = Array.from({ length: 3 }, (e) => book);
-
 function CartPage(props) {
-  const { cart = fcart } = props;
+  const { cart } = props;
 
   return (
     <Row justify='center'>
-      <Col span={24} className='bookstore-bp-column'>
-        <Breadcrumb className='bookstore-cp-breadcrumb'>
+      <Col span={24} className='bookstore-column'>
+        <Breadcrumb className='bookstore-breadcrumb'>
           <Breadcrumb.Item href='/'>Home</Breadcrumb.Item>
           <Breadcrumb.Item href='#'>Cart</Breadcrumb.Item>
         </Breadcrumb>
-        <div className='bookstore-cp-section'>
+        <div className='bookstore-page-section'>
           <div className='bookstore-cart-list'>
             <Title className='bookstore-cart-list-title'>Cart</Title>
             {cart.map((b, i) => (
