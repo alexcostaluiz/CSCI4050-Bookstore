0ae2821009020c007400917e5b2af1c902e35e72
Alexander Luiz Costa <alexcostaluiz@outlook.com>
2020-10-05 10:55:59 -0400

    Move sample data from index.js to CartContext

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,7 @@ import { Layout } from 'antd';
 
 import BookPage from './pages/BookPage.js';
 import CartPage from './pages/CartPage.js';
+import CartService from './components/CartService.js';
 import CheckoutPage from './pages/CheckoutPage.js';
 import Footer from './components/Footer.js';
 import Header from './components/Header.js';
@@ -16,46 +17,32 @@ import * as serviceWorker from './serviceWorker';
 
 const { Content } = Layout;
 
-const book = {
-  author: 'Barack Obama',
-  edition: null,
-  image: 'https://kottke.org/plus/misc/images/obama-promised-land-book.jpg',
-  isbn: '978-1-524-76316-9',
-  numRatings: 4132,
-  pages: 768,
-  price: 34.99,
-  publicationDate: 1605589200,
-  publisher: 'Crown Publishing Group',
-  rating: 4.1,
-  tags: ['Nonfiction', 'Biography', 'Autobiography', 'Bestseller'],
-  title: 'A Promised Land',
-};
-const cart = Array.from({ length: 3 }, (e) => book);
-
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <ScrollToTop />
-      <Layout>
-        <Header />
-        <Content style={{ background: 'white' }}>
-          <Switch>
-            <Route path='/b/:slug'>
-              <BookPage />
-            </Route>
-            <Route path='/cart'>
-              <CartPage cart={cart} />
-            </Route>
-            <Route path='/checkout'>
-              <CheckoutPage cart={cart} />
-            </Route>
-            <Route path='/'>
-              <HomePage />
-            </Route>
-          </Switch>
-        </Content>
-        <Footer />
-      </Layout>
+      <CartService>
+        <Layout>
+          <Header />
+          <Content style={{ background: 'white' }}>
+            <Switch>
+              <Route path='/b/:slug'>
+                <BookPage />
+              </Route>
+              <Route path='/cart'>
+                <CartPage />
+              </Route>
+              <Route path='/checkout'>
+                <CheckoutPage />
+              </Route>
+              <Route path='/'>
+                <HomePage />
+              </Route>
+            </Switch>
+          </Content>
+          <Footer />
+        </Layout>
+      </CartService>
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
