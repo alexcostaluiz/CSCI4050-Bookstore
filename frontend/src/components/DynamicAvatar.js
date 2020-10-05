a4f6e3a983a4bef77cb49a8038c090203543a819
Alexander Luiz Costa <alexcostaluiz@outlook.com>
2020-10-05 11:26:23 -0400

    Add navigation to DynamicAvatar

--- frontend/src/components/DynamicAvatar.js
+++ frontend/src/components/DynamicAvatar.js
@@ -1,19 +1,23 @@
 import React from 'react';
 
+import { useHistory } from 'react-router-dom';
+
 import { Button } from 'antd';
 
 function DynamicAvatar(props) {
   const { isSignedIn } = props;
 
+  const history = useHistory();
+
   if (isSignedIn) {
     return (
-      <Button type='primary' size='large' shape='circle'>
+      <Button type='primary' size='large' shape='circle' onclick={() => history.push('/profile')}>
         AC
       </Button>
     );
   } else {
     return (
-      <Button type='primary' size='large'>
+      <Button type='primary' size='large' onclick={() => history.push('/login')}>
         Sign In
       </Button>
     );
