20791d478bcb0bd547b412065b09bc56be0841e2
kotacode <kota@uga.edu>
2020-10-18 00:10:15 -0400

    create Promotion class

---
+++ src/main/java/com/csci4050/bookstore/model/Promotion.java
@@ -0,0 +1,34 @@
+package com.csci4050.bookstore.model;
+
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "promotion")
+public class Promotion {
+    
+    @Id
+    @Column(name = "promo_code")
+    @NotNull
+    private String promoCode;
+
+    @Column(name = "start_date")
+    @NotNull
+    private LocalDateTime startDate;
+
+    @Column(name = "end_date")
+    @NotNull
+    private LocalDateTime endDate;
+
+    @Column(name = "discount")
+    @NotNull
+    private Double discount;
+
+    public Promotion() {}
+}
