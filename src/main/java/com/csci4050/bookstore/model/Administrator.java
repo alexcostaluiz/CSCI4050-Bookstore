852a1410008a4811da02dbd2c6a128d9108b51a8
Marartu <maw11298@uga.edu>
2020-10-19 18:43:55 -0400

    mend

--- src/main/java/com/csci4050/bookstore/model/Administrator.java
+++ src/main/java/com/csci4050/bookstore/model/Administrator.java
@@ -13,36 +13,35 @@ import javax.validation.constraints.NotNull;
 @Table(name = "administrator")
 public class Administrator extends User {
 
-    @Column(name = "first_name")
-    @NotNull
-    private String firstName;
-    
-    @Column(name = "last_name")
-    @NotNull
-    private String lastName;
+  @Column(name = "first_name")
+  @NotNull
+  private String firstName;
 
-    @Column(name = "password")
-    @NotNull
-    private String password; 
+  @Column(name = "last_name")
+  @NotNull
+  private String lastName;
+
+  @Column(name = "password")
+  @NotNull
+  private String password;
 
   /*  @ElementCollection
-    @Column(name = "employees")
-    @CollectionTable(name = "employees", joinColumns = @JoinColumn(name = "id"))
-    private List<Employee> employees;
-*/
-    @ElementCollection
-    @Column(name = "books")
-    @CollectionTable(name = "books", joinColumns = @JoinColumn(name = "id"))
-    private List<Book> books;
+      @Column(name = "employees")
+      @CollectionTable(name = "employees", joinColumns = @JoinColumn(name = "id"))
+      private List<Employee> employees;
+  */
+  @ElementCollection
+  @Column(name = "books")
+  @CollectionTable(name = "books", joinColumns = @JoinColumn(name = "id"))
+  private List<Book> books;
 
-    @ElementCollection
-    @Column(name = "promotions")
-    @CollectionTable(name = "promotions", joinColumns = @JoinColumn(name = "id"))
-    private List<Promotion> promotions;
+  @ElementCollection
+  @Column(name = "promotions")
+  @CollectionTable(name = "promotions", joinColumns = @JoinColumn(name = "id"))
+  private List<Promotion> promotions;
 
-    @ElementCollection
-    @Column(name = "users")
-    @CollectionTable(name = "users", joinColumns = @JoinColumn(name = "id"))
-    private List<User> users;
-
+  @ElementCollection
+  @Column(name = "users")
+  @CollectionTable(name = "users", joinColumns = @JoinColumn(name = "id"))
+  private List<User> users;
 }
