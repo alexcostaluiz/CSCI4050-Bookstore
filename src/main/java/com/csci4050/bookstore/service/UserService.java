5747a48af895b328d4f479e7438097404eadfc0d
kotacode <kota@uga.edu>
2020-10-27 12:22:17 -0400

    Create UserService

---
+++ src/main/java/com/csci4050/bookstore/service/UserService.java
@@ -0,0 +1,39 @@
+package com.csci4050.bookstore.service;
+
+import com.csci4050.bookstore.dao.UserDAO;
+import com.csci4050.bookstore.model.User;
+import java.util.List;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserService {
+  @Autowired private UserDAO dao;
+
+  @Transactional
+  public List<User> get() {
+    return dao.get();
+  }
+
+  @Transactional
+  public User get(int id) {
+    return dao.get(id);
+  }
+
+  @Transactional
+  public void save(User user) {
+    dao.save(user);
+  }
+
+  @Transactional
+  public void delete(int id) {
+    dao.delete(id);
+  }
+
+  @Transactional
+  public void delete(User user) {
+    dao.delete(User.getId());
+  }
+}
