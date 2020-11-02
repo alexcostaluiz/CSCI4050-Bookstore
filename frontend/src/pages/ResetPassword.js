315b3e0ece5b5009c0870740c389787efd763374
Alexander Luiz Costa <alexcostaluiz@outlook.com>
2020-11-02 13:48:43 -0500

    Add reset password post-submit view

--- frontend/src/pages/ResetPassword.js
+++ frontend/src/pages/ResetPassword.js
@@ -1,121 +1,122 @@
 import './ResetPassword.less';
 
-import React from 'react';
+import React, { useState } from 'react';
+
 import {
-  Row,
+  Button,
   Col,
   Form,
   Input,
-  Button,
-  Card,
+  Row,
   Typography,
-  notification,
 } from 'antd';
 
-import { useLocation } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
 
-const { Title, Text } = Typography;
-
-const formItemLayout = {
-  labelCol: {
-    xs: { span: 24 },
-    sm: { span: 8 },
-  },
-  wrapperCol: {
-    xs: { span: 24 },
-    sm: { span: 16 },
-  },
-};
+const { Paragraph, Title } = Typography;
 
 function ResetPassword(props) {
+  const history = useHistory();
   const location = useLocation();
   const [form] = Form.useForm();
-  const openNotification = () => {
-    notification.open({
-      message: 'Password Reset',
-      onClick: () => {
-        console.log('Notification Clicked!');
-      },
-    });
-  };
 
-  const onFinish = (values) => {
+  const [loading, setLoading] = useState(false);
+  const [submitted, setSubmitted] = useState(false);
+  
+  const onFinish = async (values) => {
+    setLoading(true);
+    
     delete values.confirm;
     values['token'] = location.search.split('=')[1];
-    fetch('/registration/savePassword', {
+    await fetch('/registration/savePassword', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(values),
     });
-    openNotification();
+
+    setLoading(false);
+    setSubmitted(true);
   };
+
+  const content = submitted ? (
+    <div className='bookstore-reset-password-container'>
+      <Title className='bookstore-reset-password-title'>Reset Password</Title>
+      <Paragraph>
+        Your password has been successfully reset! You may now sign in using your
+        new password.
+      </Paragraph>
+      <Button
+        type='primary'
+        htmlType='submit'
+        size='large'
+        onClick={() => history.push('/login')}>
+        SIGN IN
+      </Button>
+    </div>
+  ) : (
+    <div className='bookstore-reset-password-container'>
+      <Title className='bookstore-reset-password-title'>Reset Password</Title>
+      <Paragraph>
+        Please enter and confirm your new password below.
+      </Paragraph>
+      <Form
+        form={form}
+        name='reset-password'
+        className='bookstore-reset-password-form'
+        onFinish={onFinish}>
+        <Form.Item
+          name='newPassword'
+          rules={[
+            {
+              required: true,
+              message: 'Please input a new password',
+            },
+          ]}
+          hasFeedback>
+          <Input.Password placeholder='Password'/>
+        </Form.Item>
+        <Form.Item
+          name='confirm'
+          dependencies={['newPassword']}
+          hasFeedback
+          rules={[
+            {
+              required: true,
+              message: 'Please confirm your new password',
+            },
+            ({ getFieldValue }) => ({
+              validator(rule, value) {
+                if (!value || getFieldValue('newPassword') === value) {
+                  return Promise.resolve();
+                }
+                return Promise.reject(
+                  'The two passwords do not match'
+                );
+              },
+            }),
+          ]}>
+          <Input.Password placeholder='Confirm Password'/>
+        </Form.Item>
+        <Form.Item style={{ marginBottom: '0px' }}>
+          <Button
+            type='primary'
+            htmlType='submit'
+            size='large'
+            loading={loading}>
+            RESET PASSWORD
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
 
   return (
     <Row justify='center'>
       <Col className='bookstore-column'>
         <div className='bookstore-page-section'>
-          <Card className='reset-password-container'>
-            <Title className='reset-password-title'>Reset Password</Title>
-            <Text>
-              Please enter and confirm your new password below to access your
-              account.
-            </Text>
-            <Form
-              {...formItemLayout}
-              form={form}
-              name='reset-password'
-              className='reset-password'
-              onFinish={onFinish}
-              scrollToFirstError>
-              <Form.Item
-                name='newPassword'
-                label='Password'
-                rules={[
-                  {
-                    required: true,
-                    message: 'Please input your password!',
-                  },
-                ]}
-                hasFeedback>
-                <Input.Password />
-              </Form.Item>
-              <Form.Item
-                name='confirm'
-                label='Confirm Password'
-                dependencies={['newPassword']}
-                hasFeedback
-                rules={[
-                  {
-                    required: true,
-                    message: 'Please confirm your password!',
-                  },
-                  ({ getFieldValue }) => ({
-                    validator(rule, value) {
-                      if (!value || getFieldValue('newPassword') === value) {
-                        return Promise.resolve();
-                      }
-                      return Promise.reject(
-                        'The two passwords that you entered do not match!'
-                      );
-                    },
-                  }),
-                ]}>
-                <Input.Password />
-              </Form.Item>
-              <Form.Item>
-                <Button
-                  type='primary'
-                  htmlType='submit'
-                  className='reset-password-button'
-                  size='large'
-                  block>
-                  Reset Password
-                </Button>
-              </Form.Item>
-            </Form>
-          </Card>
+          {content}
         </div>
       </Col>
     </Row>
