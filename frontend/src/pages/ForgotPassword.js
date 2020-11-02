a5873ec9d0a8dc620e96dc0fe2f00b19889d8cff
Alexander Luiz Costa <alexcostaluiz@outlook.com>
2020-11-02 13:47:41 -0500

    Add forgot password post-submit view

--- frontend/src/pages/ForgotPassword.js
+++ frontend/src/pages/ForgotPassword.js
@@ -1,6 +1,6 @@
 import './ForgotPassword.less';
 
-import React from 'react';
+import React, { useState } from 'react';
 
 import {
   Row,
@@ -8,7 +8,6 @@ import {
   Form,
   Input,
   Button,
-  notification,
   Typography,
 } from 'antd';
 
@@ -17,63 +16,83 @@ const { Paragraph, Title } = Typography;
 function ForgotPassword(props) {
   const [form] = Form.useForm();
 
-  const onFinish = (values) => {
-    fetch('/auth/forgot_password', {
+  const [loading, setLoading] = useState(false);
+  const [submitted, setSubmitted] = useState(null);
+
+  const onFinish = async (values) => {
+    setLoading(true);
+    await fetch('/auth/forgot_password', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(values),
     });
-    notification.open({
-      message: 'Email Sent',
-      description: 'Click the link in your email to reset your password.',
-    });
+    setLoading(false);
+    setSubmitted(values);
   };
+
+  const content = submitted ? (
+    <div className='bookstore-forgot-password-container'>      
+      <Title className='bookstore-forgot-password-title'>Forgot Password</Title>
+      <Paragraph>
+        A password reset link has been successfully emailed to <b>{submitted.emailAddress}</b>.
+        Please follow the link to reset your password. If you cannot find the email you may
+        try resending the reset link.
+      </Paragraph>      
+      <Button
+        type='primary'
+        onClick={() => onFinish(submitted)}
+        loading={loading}
+        size='large'>
+        RESEND EMAIL
+      </Button>
+    </div>
+  ) : (
+    <div className='bookstore-forgot-password-container'>
+      <Title className='bookstore-forgot-password-title'>Forgot Password</Title>
+      <Paragraph>
+        If you have forgotten your password, enter the email address
+        you used to register for an account below and a link to reset
+        your password will be sent to the address.
+      </Paragraph>
+      <Form
+        form={form}
+        name='forgot-password'
+        className='bookstore-forgot-password-form'
+        onFinish={onFinish}>
+        <Form.Item
+          name='emailAddress'
+          rules={[
+            {
+              type: 'email',
+              message: 'The input provided is not a valid email address',
+            },
+            {
+              required: true,
+              message: 'Please input an email address',
+            },
+          ]}>
+          <Input placeholder='Email Address' />
+        </Form.Item>
+        <Form.Item style={{ marginBottom: '0px' }}>
+          <Button
+            type='primary'
+            htmlType='submit'
+            size='large'
+            loading={loading}>
+            RESET PASSWORD
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
 
   return (
     <Row justify='center'>
       <Col className='bookstore-column'>
         <div className='bookstore-page-section'>
-          <div className='bookstore-forgot-password-container'>
-            <Title className='bookstore-forgot-password-title'>Forgot Password</Title>
-            <Paragraph>
-              If you have forgotten your password, enter the email address
-              you used to register for an account below and a link to reset
-              your password will be sent to the address.
-            </Paragraph>
-            <Form
-              form={form}
-              name='forgot-password'
-              className='bookstore-forgot-password-form'
-              onFinish={onFinish}
-              scrollToFirstError>
-              <Form.Item
-                name='emailAddress'
-                rules={[
-                  {
-                    type: 'email',
-                    message: 'The input provided is not a valid email address',
-                  },
-                  {
-                    required: true,
-                    message: 'Please input an email address',
-                  },
-                ]}>
-                <Input placeholder='Email Address' />
-              </Form.Item>
-              <Form.Item>
-                <Button
-                  type='primary'
-                  htmlType='submit'
-                  className='bookstore-forgot-password-form-button'
-                  size='large'
-                  block>
-                  Reset Password
-                </Button>
-              </Form.Item>
-            </Form>
-          </div>
+          {content}
         </div>
       </Col>
     </Row>
