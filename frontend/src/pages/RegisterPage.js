f9bee5b645524d4b8aae910886c5f89a2252e531
Alexander Luiz Costa <alexcostaluiz@outlook.com>
2020-11-02 13:48:18 -0500

    Style Register page

--- frontend/src/pages/RegisterPage.js
+++ frontend/src/pages/RegisterPage.js
@@ -1,203 +1,189 @@
 import './RegisterPage.less';
 
-import React from 'react';
+import React, { useState } from 'react';
 
 import { useHistory } from 'react-router-dom';
 
 import {
-  Row,
+  Button,
+  Checkbox,
   Col,
   Form,
   Input,
-  Checkbox,
-  Button,
-  notification,
+  Row,
   Typography,
 } from 'antd';
 
-const { Title } = Typography;
-
-const formItemLayout = {
-  labelCol: {
-    xs: { span: 24 },
-    sm: { span: 8 },
-  },
-  wrapperCol: {
-    xs: { span: 24 },
-    sm: { span: 16 },
-  },
-};
-const tailFormItemLayout = {
-  wrapperCol: {
-    xs: {
-      span: 24,
-      offset: 0,
-    },
-    sm: {
-      span: 16,
-      offset: 8,
-    },
-  },
-};
+const { Paragraph, Title } = Typography;
 
 function Register(props) {
+  const history = useHistory();
   const [form] = Form.useForm();
 
-  const history = useHistory();
+  const [loading, setLoading] = useState(false);
+  const [submitted, setSubmitted] = useState(null);
 
-  const onFinish = (values) => {
+  const onFinish = async (values) => {
+    setLoading(true);
+    
     delete values.confirm;
-    fetch('/auth/register', {
+    await fetch('/auth/register', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(values),
     });
-    notification.open({
-      message: 'Registration Complete',
-      description:
-      'Confirmation email sent. Please use the link to activate your account.',
-    });
+    
+    setLoading(false);
+    setSubmitted(values);
   };
+
+  const content = submitted ? (
+    <div className='bookstore-register-container'>
+      <Title className='bookstore-register-title' style={{ marginBottom: '8px' }}>
+        Welcome, {submitted.firstName}!
+      </Title>
+      <Paragraph>
+        Your account has been successfully created! A confirmation email has been sent
+        to <b>{submitted.emailAddress}</b> with a link to confirm your email address.
+        Once you have confirmed your email address, you may sign in.
+      </Paragraph>
+      <Button
+        type='primary'
+        size='large'
+        onClick={() => history.push('/login')}>
+        SIGN IN
+      </Button>
+    </div>
+  ) : (
+    <div className='bookstore-register-container'>
+      <Title className='bookstore-register-title'>Register</Title>
+      <Form
+        form={form}
+        name='register'
+        className='bookstore-register-form'
+        onFinish={onFinish}>
+        <Form.Item
+          name='firstName'
+          rules={[
+            {
+              required: true,
+              message: 'Please input your first name',
+            },
+          ]}>
+          <Input placeholder='First Name' />
+        </Form.Item>
+        <Form.Item
+          name='lastName'
+          rules={[
+            {
+              required: true,
+              message: 'Please input your last name',
+            },
+          ]}>
+          <Input placeholder='Last Name'/>
+        </Form.Item>
+        <Form.Item
+          name='emailAddress'
+          rules={[
+            {
+              type: 'email',
+              message: 'The input provided is not a valid email address',
+            },
+            {
+              required: true,
+              message: 'Please input an email address',
+            },
+          ]}>
+          <Input placeholder='Email Address'/>
+        </Form.Item>
+        <Form.Item
+          name='phoneNumber'
+          rules={[
+            {
+              required: true,
+              message: 'Please input a phone number',
+            },
+            {
+              pattern: '[0-9]{3}[0-9]{3}[0-9]{4}',
+              max: 10,
+              message: 'The input provided is not a valid phone number',
+            },
+          ]}>
+          <Input placeholder='Phone Number'/>
+        </Form.Item>
+        <Form.Item
+          name='password'
+          rules={[
+            {
+              required: true,
+              message: 'Please input a password',
+            },
+          ]}
+          hasFeedback>
+          <Input.Password placeholder='Password' />
+        </Form.Item>
+        <Form.Item
+          name='confirm'
+          dependencies={['password']}
+          hasFeedback
+          rules={[
+            {
+              required: true,
+              message: 'Please confirm your password',
+            },
+            ({ getFieldValue }) => ({
+              validator(rule, value) {
+                if (!value || getFieldValue('password') === value) {
+                  return Promise.resolve();
+                }
+                return Promise.reject(
+                  'The two passwords do not match'
+                );
+              },
+            }),
+          ]}>
+          <Input.Password placeholder='Confirm Password' />
+        </Form.Item>
+        <Form.Item name='subscription'>
+          <Checkbox defaultChecked={true}>Sign up for promotional emails</Checkbox>
+        </Form.Item>
+        <Paragraph>
+          By clicking "Register" below, you agree to the Bookstore{' '}
+          <Button type='link' style={{ padding: '0px', height: 'unset' }}>
+            Terms of Service
+          </Button>
+          {' '}and{' '}
+          <Button type='link' style={{ padding: '0px', height: 'unset' }}>
+            Privacy Policy
+          </Button>.
+        </Paragraph>
+        <Form.Item style={{ marginBottom: '0px' }}>
+          <Button
+            type='primary'
+            htmlType='submit'
+            className='bookstore-register-form-button'
+            size='large'
+            loading={loading}
+            block
+            onSubmit={() => history.push('/login')}>
+            REGISTER
+          </Button>
+        </Form.Item>
+        Already have an account?{' '}
+        <Button onClick={() => history.push('/login')} type='link' style={{ padding: '0px'}}>
+          Sign In
+        </Button>
+      </Form>
+    </div>
+  );
 
   return (
     <Row justify='center'>
       <Col className='bookstore-column'>
         <div className='bookstore-page-section'>
-          <div className='bookstore-register-container'>
-            <Title className='bookstore-register-title'>Register</Title>
-            <Form
-              {...formItemLayout}
-              form={form}
-              name='register'
-              className='bookstore-register-form'
-              onFinish={onFinish}
-              scrollToFirstError>
-              <Form.Item
-                label='First Name'
-                name='firstName'
-                rules={[
-                  {
-                    required: true,
-                    message: 'Please input your first name',
-                  },
-                ]}>
-                <Input />
-              </Form.Item>
-              <Form.Item
-                label='Last Name'
-                name='lastName'
-                rules={[
-                  {
-                    required: true,
-                    message: 'Please input your last name',
-                  },
-                ]}>
-                <Input />
-              </Form.Item>
-              <Form.Item
-                name='emailAddress'
-                label='E-mail'
-                rules={[
-                  {
-                    type: 'email',
-                    message: 'The input provided is not a valid email address',
-                  },
-                  {
-                    required: true,
-                    message: 'Please input an email address',
-                  },
-                ]}>
-                <Input />
-              </Form.Item>
-              <Form.Item name='subscription' {...tailFormItemLayout}>
-                <Checkbox defaultChecked={true}>Send me promotions!</Checkbox>
-              </Form.Item>
-              <Form.Item
-                label='Phone number'
-                name='phoneNumber'
-                rules={[
-                  {
-                    required: true,
-                    message: 'Please input a phone number',
-                  },
-                  {
-                    pattern: '[0-9]{3}[0-9]{3}[0-9]{4}',
-                    max: 10,
-                    message: 'The input provided is not a vaild phone number',
-                  },
-                ]}>
-                <Input />
-              </Form.Item>
-              <Form.Item
-                name='password'
-                label='Password'
-                rules={[
-                  {
-                    required: true,
-                    message: 'Please input a password',
-                  },
-                ]}
-                hasFeedback>
-                <Input.Password />
-              </Form.Item>
-              <Form.Item
-                name='confirm'
-                label='Confirm Password'
-                dependencies={['password']}
-                hasFeedback
-                rules={[
-                  {
-                    required: true,
-                    message: 'Please confirm your password',
-                  },
-                  ({ getFieldValue }) => ({
-                    validator(rule, value) {
-                      if (!value || getFieldValue('password') === value) {
-                        return Promise.resolve();
-                      }
-                      return Promise.reject(
-                        'The two passwords do not match'
-                      );
-                    },
-                  }),
-                ]}>
-                <Input.Password />
-              </Form.Item>
-              <Form.Item
-                name='subscription'
-                valuePropName='checked'
-                rules={[
-                  {
-                    validator: (_, value) =>
-                      value
-                        ? Promise.resolve()
-                        : Promise.reject('You must accept the agreement'),
-                  },
-                ]}
-                {...tailFormItemLayout}>
-                <Checkbox>
-                  I have read the <Button type='link'>agreement</Button>
-                </Checkbox>
-              </Form.Item>
-              <Form.Item>
-                <Button
-                  type='primary'
-                  htmlType='submit'
-                  className='bookstore-register-form-button'
-                  size='large'
-                  block
-                  onSubmit={() => history.push('/login')}>
-                  Register
-                </Button>
-              </Form.Item>
-              Already have an account?
-              <Button onClick={() => history.push('/login')} type='link'>
-                Sign In
-              </Button>
-            </Form>
-          </div>
+          {content}
         </div>
       </Col>
     </Row>
