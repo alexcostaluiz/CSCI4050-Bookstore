ec6a1fc95df7fa880e2f595d0e0ae106c2bc7ffc
Lennon Scariano <lennon.scariano@uga.edu>
2020-11-14 00:12:21 -0500

    fixed user query error

--- src/main/java/com/csci4050/bookstore/dao/UserDAO.java
+++ src/main/java/com/csci4050/bookstore/dao/UserDAO.java
@@ -3,37 +3,39 @@ package com.csci4050.bookstore.dao;
 import com.csci4050.bookstore.model.User;
 import java.util.List;
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 import javax.transaction.Transactional;
 import org.hibernate.Session;
 import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
-@Transactional
 public class UserDAO implements DAO<User> {
-  @PersistenceContext private EntityManager entityManager;
+  @Autowired private EntityManager entityManager;
 
   /* Return a list of users */
   @Override
+  @Transactional
   public List<User> get() {
     Session session = entityManager.unwrap(Session.class);
-    Query<User> query = session.createQuery("FROM user", User.class);
+    Query<User> query = session.createQuery("FROM User", User.class);
     List<User> users = query.getResultList();
     return users;
   }
 
   /* Return a single user */
   @Override
+  @Transactional
   public User get(int id) {
     Session session = entityManager.unwrap(Session.class);
     User user = session.get(User.class, id);
     return user;
   }
 
+  @Transactional
   public User get(String email) {
     Session session = entityManager.unwrap(Session.class);
     CriteriaBuilder cb = session.getCriteriaBuilder();
@@ -49,12 +51,14 @@ public class UserDAO implements DAO<User> {
   }
 
   /* Update user */
+  @Transactional
   public void update(User user) {
     Session session = entityManager.unwrap(Session.class);
     session.update(user);
   }
 
   @Override
+  @Transactional
   public void save(User user) {
     Session session = entityManager.unwrap(Session.class);
     session.save(user);
@@ -62,6 +66,7 @@ public class UserDAO implements DAO<User> {
 
   /* Delete a user */
   @Override
+  @Transactional
   public void delete(int id) {
     Session session = entityManager.unwrap(Session.class);
     User user = session.get(User.class, id);
