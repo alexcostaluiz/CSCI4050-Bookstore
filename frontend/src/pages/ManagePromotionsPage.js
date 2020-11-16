bcbcb836fdd801e1378f06653098bb7d01e4ce63
Alexander Luiz Costa <alexcostaluiz@outlook.com>
2020-11-16 02:49:25 -0500

    Create ManageUsersPage and ManagePromotionsPage components

---
+++ frontend/src/pages/ManagePromotionsPage.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { Table } from 'antd';
+
+import ManagePage from './ManagePage.js';
+
+function PromotionTable(props) {
+  return (
+    <Table />
+  );
+}
+
+function ManagePromotionsPage(props) {
+  return (
+    <ManagePage
+      title='Manage Promotions'
+      shortTitle='Promotions'
+      table={<PromotionTable />}
+    />
+  );
+}
+
+export default ManagePromotionsPage;
---
+++ frontend/src/pages/ManageUsersPage.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { Table } from 'antd';
+
+import ManagePage from './ManagePage.js';
+
+function UserTable(props) {
+  return (
+    <Table />
+  );
+}
+
+function ManageUsersPage(props) {
+  return (
+    <ManagePage
+      title='Manage Users'
+      shortTitle='Users'
+      table={<UserTable />}
+    />
+  );
+}
+
+export default ManageUsersPage;
