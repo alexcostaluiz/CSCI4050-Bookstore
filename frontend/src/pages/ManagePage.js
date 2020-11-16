4c3db09eabe7bd83b6235b729cf7c981c6c831e2
Alexander Luiz Costa <alexcostaluiz@outlook.com>
2020-11-16 02:49:05 -0500

    Create generic ManagePage component

---
+++ frontend/src/pages/ManagePage.js
@@ -0,0 +1,48 @@
+import './ManagePage.less';
+
+import React from 'react';
+
+import { Link } from 'react-router-dom';
+
+import { Breadcrumb, Button, Col, Row, Typography } from 'antd';
+
+const { Title } = Typography;
+
+function ManagePage(props) {
+  const { table, title, shortTitle } = props;
+  
+  return (
+    <Row justify='center'>
+      <Col span={24} className='bookstore-column'>
+        <Breadcrumb className='bookstore-breadcrumb'>
+          <Breadcrumb.Item>
+            <Link to='/admin'>Admin</Link>
+          </Breadcrumb.Item>
+          <Breadcrumb.Item>
+            <Link to='/admin'>Manage</Link>
+          </Breadcrumb.Item>
+          <Breadcrumb.Item>
+            <Link to=''>{shortTitle}</Link>
+          </Breadcrumb.Item>
+        </Breadcrumb>
+        <div className='bookstore-page-section'>
+          <div className='bookstore-manage-table-container'>
+            <div className='bookstore-manage-table-title-container'>
+              <Title className='bookstore-manage-table-title'>
+                {title}
+              </Title>
+              <Button type='primary' size='large'>
+                ADD {shortTitle.slice(0, -1).toUpperCase()}
+              </Button>
+            </div>
+            <div className='bookstore-manage-table-wrapper'>
+              {table}
+            </div>
+          </div>
+        </div>
+      </Col>
+    </Row>
+  );
+}
+
+export default ManagePage;
