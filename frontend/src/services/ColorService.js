6c29845bc29b097e566f1b8c96da84d69a934e64
Alexander Luiz Costa <alexcostaluiz@outlook.com>
2020-11-19 15:21:27 -0500

    Add ColorService

---
+++ frontend/src/services/ColorService.js
@@ -0,0 +1,7 @@
+const Colors = {
+  colors: ['magenta', 'red', 'volcano', 'orange', 'gold', 'lime', 'green', 'cyan',
+           'blue', 'geekblue', 'purple'],
+  category: (cat) => Colors.colors[cat.charCodeAt(0) % 11],
+};
+
+export default Colors;
