2a9959ab1a029bebff917e24f3242ace7d482d75
Lennon Scariano <lennon.scariano@uga.edu>
2020-11-18 21:55:07 -0500

    moved files in project root to respective folders

---
+++ src/main/java/com/csci4050/bookstore/config/WebSecurityConfig.java
@@ -0,0 +1,84 @@
+package com.csci4050.bookstore.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Autowired UserDetailsService userDetailsService;
+  @Autowired AuthenticationFailureHandler failureHandler;
+  @Autowired AuthenticationSuccessHandler successHandler;
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.csrf()
+        .disable()
+        .authorizeRequests()
+        .antMatchers(
+            "/profile",
+            "/checkout",
+            "/logout",
+            "/edit/changePassword",
+            "/edit/personalInfo",
+            "/edit/saveCard",
+            "/edit/deleteCard",
+            "/edit/updateAddress")
+        .hasAnyAuthority("ADMIN", "USER")
+        .antMatchers(
+            "/admin",
+            "/admin/manage/books",
+           // "/books/create",
+            "/books/update",
+            "/books/archive",
+            "/promos/update",
+            "/promos/delete",
+            "/promos/create",
+            "/promos/email")
+        .hasAuthority("ADMIN")
+        .antMatchers("/", "/cart", "/b/**")
+        .permitAll()
+        .and()
+        .formLogin()
+        .loginPage("/login")
+        .failureHandler(failureHandler)
+        .successHandler(successHandler)
+        .permitAll()
+        .and()
+        .rememberMe()
+        .key("ourKey")
+        .rememberMeParameter("remember")
+        .and()
+        .logout()
+        .logoutSuccessUrl("/login")
+        .invalidateHttpSession(true);
+  }
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.userDetailsService(userDetailsService);
+  }
+
+  @Bean("authenticationManager")
+  @Override
+  public AuthenticationManager authenticationManagerBean() throws Exception {
+    return super.authenticationManagerBean();
+  }
+
+  @Bean
+  public PasswordEncoder getPasswordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+}
---
+++ src/main/java/com/csci4050/bookstore/converter/CardNumberConverter.java
@@ -0,0 +1,22 @@
+package com.csci4050.bookstore.converter;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+import org.springframework.security.crypto.encrypt.Encryptors;
+import org.springframework.security.crypto.encrypt.TextEncryptor;
+
+@Converter
+public class CardNumberConverter implements AttributeConverter<String, String> {
+  String salt = "73616c74"; // KeyGenerators.string().generateKey();
+  TextEncryptor enc = Encryptors.text("password", salt);
+
+  @Override
+  public String convertToDatabaseColumn(String attribute) {
+    return enc.encrypt(attribute);
+  }
+
+  @Override
+  public String convertToEntityAttribute(String dbData) {
+    return enc.decrypt(dbData);
+  }
+}
