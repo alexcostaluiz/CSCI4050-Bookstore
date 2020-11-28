c0d18a71df1cbbfb8bef19fd838efab6211fff02
Lennon Scariano <lennon.scariano@uga.edu>
2020-11-28 14:47:53 -0500

    changed to localdatetime

--- src/main/java/com/csci4050/bookstore/dto/OrderDTO.java
+++ src/main/java/com/csci4050/bookstore/dto/OrderDTO.java
@@ -3,14 +3,17 @@ package com.csci4050.bookstore.dto;
 import com.csci4050.bookstore.model.Address;
 import com.csci4050.bookstore.model.Card;
 import com.csci4050.bookstore.model.Promotion;
-import java.time.LocalDate;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.time.LocalDateTime;
 
-public class OrderDTO {
+public class OrderDto {
 
   private Address address;
   private Card payment;
   private Promotion promo;
-  private LocalDate orderDate;
+
+  @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+  private LocalDateTime orderDate;
 
   public Address getAddress() {
     return this.address;
@@ -36,11 +39,11 @@ public class OrderDTO {
     this.promo = promo;
   }
 
-  public LocalDate getOrderDate() {
+  public LocalDateTime getOrderDate() {
     return this.orderDate;
   }
 
-  public void setOrderDate(LocalDate orderDate) {
+  public void setOrderDate(LocalDateTime orderDate) {
     this.orderDate = orderDate;
   }
 }
