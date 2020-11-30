2c4a1d36e2f6f6444e90120de79e8889721ad50a
Lennon Scariano <lennon.scariano@uga.edu>
2020-11-30 13:14:09 -0500

    made pretty

--- src/main/java/com/csci4050/bookstore/controller/CartController.java
+++ src/main/java/com/csci4050/bookstore/controller/CartController.java
@@ -1,7 +1,5 @@
 package com.csci4050.bookstore.controller;
 
-import java.util.Map;
-import javax.annotation.Resource;
 import com.csci4050.bookstore.dto.BookDto;
 import com.csci4050.bookstore.dto.CartDto;
 import com.csci4050.bookstore.dto.CartItemDto;
@@ -13,6 +11,8 @@ import com.csci4050.bookstore.model.User;
 import com.csci4050.bookstore.service.BookService;
 import com.csci4050.bookstore.service.OrderService;
 import com.csci4050.bookstore.service.UserService;
+import java.util.Map;
+import javax.annotation.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
@@ -30,137 +30,143 @@ import org.springframework.web.server.ResponseStatusException;
 @RequestMapping("/cart")
 public class CartController {
 
-    @Resource(name = "authenticationManager")
-    AuthenticationManager authManager;
+  @Resource(name = "authenticationManager")
+  AuthenticationManager authManager;
 
-    @Autowired private UserService userService;
-    @Autowired private OrderService orderService;
-    @Autowired private BookService bookService;
-    @Autowired private ApplicationEventPublisher eventPublisher;
+  @Autowired private UserService userService;
+  @Autowired private OrderService orderService;
+  @Autowired private BookService bookService;
+  @Autowired private ApplicationEventPublisher eventPublisher;
 
-    /**
-     * adds a book to the cart with desired quantity
-     * @param dto
-     */
-    @PostMapping("/add")
-    public void add(@RequestBody CartItemDto dto) {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        if (auth != null) {
-            Object principal = auth.getPrincipal();
-            if (principal instanceof UserDetails) {
-                User user = userService.getUser(((UserDetails) principal).getUsername());
-                if(dto.getBook() == null) {
-                    throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Book doesn't exist");
-                }
-                Book book = bookService.get(dto.getBook().getId());
-                Map<Book, Integer> map = user.getCart();
-                map.put(book, dto.getQuantity());
-                user.setCart(map);
-                userService.updateUser(user);
-            }
+  /**
+   * adds a book to the cart with desired quantity
+   *
+   * @param dto
+   */
+  @PostMapping("/add")
+  public void add(@RequestBody CartItemDto dto) {
+    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+    if (auth != null) {
+      Object principal = auth.getPrincipal();
+      if (principal instanceof UserDetails) {
+        User user = userService.getUser(((UserDetails) principal).getUsername());
+        if (dto.getBook() == null) {
+          throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Book doesn't exist");
         }
+        Book book = bookService.get(dto.getBook().getId());
+        Map<Book, Integer> map = user.getCart();
+        map.put(book, dto.getQuantity());
+        user.setCart(map);
+        userService.updateUser(user);
+      }
     }
+  }
 
-    /**
-     * removes book entirely from cart
-     * @param dto
-     */
-    @PostMapping("/remove")
-    public void remove(@RequestBody BookDto dto) {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        if (auth != null) {
-            Object principal = auth.getPrincipal();
-            if (principal instanceof UserDetails) {
-                User user = userService.getUser(((UserDetails) principal).getUsername());
-                Map<Book, Integer> map = user.getCart();
-                Book book = bookService.get(dto.getId());
-                if(map.remove(book) == null) {//no mapping found
-                    throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Book is not in cart.");
-                }
-                user.setCart(map);
-                userService.updateUser(user);
-            }
+  /**
+   * removes book entirely from cart
+   *
+   * @param dto
+   */
+  @PostMapping("/remove")
+  public void remove(@RequestBody BookDto dto) {
+    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+    if (auth != null) {
+      Object principal = auth.getPrincipal();
+      if (principal instanceof UserDetails) {
+        User user = userService.getUser(((UserDetails) principal).getUsername());
+        Map<Book, Integer> map = user.getCart();
+        Book book = bookService.get(dto.getId());
+        if (map.remove(book) == null) { // no mapping found
+          throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Book is not in cart.");
         }
+        user.setCart(map);
+        userService.updateUser(user);
+      }
     }
+  }
 
-    /**
-     * updates quantity of books
-     * @param dto
-     */
-    @PostMapping("/updateQuantity")
-    public void updateQuantity(@RequestBody CartItemDto dto) {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        if (auth != null) {
-            Object principal = auth.getPrincipal();
-            if (principal instanceof UserDetails) {
-                User user = userService.getUser(((UserDetails) principal).getUsername());
-                Book book = bookService.get(dto.getBook().getId());
-                if(user.getCart().containsKey(book)) {
-                    add(dto); //add method can also act as a replace
-                } else {
-                    throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Book is not in cart.");
-                }
-            }
+  /**
+   * updates quantity of books
+   *
+   * @param dto
+   */
+  @PostMapping("/updateQuantity")
+  public void updateQuantity(@RequestBody CartItemDto dto) {
+    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+    if (auth != null) {
+      Object principal = auth.getPrincipal();
+      if (principal instanceof UserDetails) {
+        User user = userService.getUser(((UserDetails) principal).getUsername());
+        Book book = bookService.get(dto.getBook().getId());
+        if (user.getCart().containsKey(book)) {
+          add(dto); // add method can also act as a replace
+        } else {
+          throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Book is not in cart.");
         }
+      }
     }
-    /**
-     * Does a full update of the cart.
-     * @param cart
-     */
-    @PostMapping("/updateCart")
-    public void updateCart(@RequestBody CartDto cart) {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+  }
+  /**
+   * Does a full update of the cart.
+   *
+   * @param cart
+   */
+  @PostMapping("/updateCart")
+  public void updateCart(@RequestBody CartDto cart) {
+    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 
-        if (auth != null) {
-            Object principal = auth.getPrincipal();
-            if (principal instanceof UserDetails) {
-                User user = userService.getUser(((UserDetails) principal).getUsername());
-                user.setCart(cart.getBooks());
-                userService.updateUser(user);
-            }
+    if (auth != null) {
+      Object principal = auth.getPrincipal();
+      if (principal instanceof UserDetails) {
+        User user = userService.getUser(((UserDetails) principal).getUsername());
+        user.setCart(cart.getBooks());
+        userService.updateUser(user);
+      }
+    }
+  }
+
+  /**
+   * Clears the cart and saves the order
+   *
+   * @param dto
+   */
+  @PostMapping(
+      value = "/checkout",
+      consumes = {"application/json"})
+  public void checkout(@RequestBody OrderDto dto) {
+    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+    if (auth != null) {
+      Object principal = auth.getPrincipal();
+      if (principal instanceof UserDetails) {
+        User user = userService.getUser(((UserDetails) principal).getUsername());
+
+        // reset cart, add order to history and send email
+        if (user.getCart().size() > 0) {
+          Map<Book, Integer> cart = user.getCart();
+
+          // setup order object to be persisted
+          Order order = new Order();
+          order.setAddress(dto.getAddress());
+          order.setOrderCart(cart);
+          order.setOrderDate(dto.getOrderDate());
+          order.setPayment(dto.getPayment());
+          order.setPromo(dto.getPromo());
+          order.setUser(user);
+
+          // reset cart
+          cart.clear();
+          user.setCart(cart);
+          userService.updateUser(user);
+
+          // persist order
+          int orderId = orderService.save(order);
+
+          // send email
+          eventPublisher.publishEvent(new OrderEvent(orderService.get(orderId), user));
+        } else {
+          throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Cart is empty.");
         }
+      }
     }
-
-    /**
-     * Clears the cart and saves the order
-     * @param dto
-     */
-    @PostMapping(value = "/checkout", consumes = {"application/json"})
-    public void checkout(@RequestBody OrderDto dto) {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        if (auth != null) {
-            Object principal = auth.getPrincipal();
-            if (principal instanceof UserDetails) {
-                User user = userService.getUser(((UserDetails) principal).getUsername());
-
-                // reset cart, add order to history and send email
-                if (user.getCart().size() > 0) {
-                    Map<Book, Integer> cart = user.getCart();
-
-                    // setup order object to be persisted
-                    Order order = new Order();
-                    order.setAddress(dto.getAddress());
-                    order.setOrderCart(cart);
-                    order.setOrderDate(dto.getOrderDate());
-                    order.setPayment(dto.getPayment());
-                    order.setPromo(dto.getPromo());
-                    order.setUser(user);
-
-                    // reset cart
-                    cart.clear();
-                    user.setCart(cart);
-                    userService.updateUser(user);
-
-                    // persist order
-                    int orderId = orderService.save(order);
-
-                    // send email
-                    eventPublisher.publishEvent(new OrderEvent(orderService.get(orderId), user));
-                } else {
-                    throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Cart is empty.");
-                }
-            }
-        }
-    }
-    
+  }
 }
--- src/main/java/com/csci4050/bookstore/dto/CartItemDto.java
+++ src/main/java/com/csci4050/bookstore/dto/CartItemDto.java
@@ -3,23 +3,22 @@ package com.csci4050.bookstore.dto;
 import com.csci4050.bookstore.model.Book;
 
 public class CartItemDto {
-    private Book book;
-    private Integer quantity;
+  private Book book;
+  private Integer quantity;
 
+  public Book getBook() {
+    return this.book;
+  }
 
-    public Book getBook() {
-        return this.book;
-    }
+  public void setBook(Book book) {
+    this.book = book;
+  }
 
-    public void setBook(Book book) {
-        this.book = book;
-    }
+  public Integer getQuantity() {
+    return this.quantity;
+  }
 
-    public Integer getQuantity() {
-        return this.quantity;
-    }
-
-    public void setQuantity(Integer quantity) {
-        this.quantity = quantity;
-    }
+  public void setQuantity(Integer quantity) {
+    this.quantity = quantity;
+  }
 }
