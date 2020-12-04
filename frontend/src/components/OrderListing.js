f08ccb1b9ee655dcbb8e2e511e239e3054b34782
Lennon Scariano <lennon.scariano@uga.edu>
2020-12-04 12:14:51 -0500

    fixed some bugs

--- frontend/src/components/OrderListing.js
+++ frontend/src/components/OrderListing.js
@@ -1,8 +1,8 @@
 import '../pages/CheckoutPage.less';
 
-import React, { useState } from 'react';
+import React from 'react';
 
-import { Breadcrumb, Col, Row, Typography } from 'antd';
+import { Breadcrumb, Col, Row, Spin, Typography } from 'antd';
 
 import CartList from '../components/CartList.js';
 import CartSummary from '../components/CartSummary.js';
@@ -12,23 +12,20 @@ const { Paragraph, Title } = Typography;
 
 function OrderListing(props) {
   const { order } = props;
-  const [popOrder, setPopOrder] = useState(order);
-
   (async () => {
-    let array = [];
+    let cart = [];
     for (var item in order.orderCart) {
-      console.log(item.book.id);
-      const book = await DB.fetchBook(item.book.id);
+      const book = await DB.fetchBook(item);
       const obj = {
         book: book,
-        quantity: item.quantity,
+        quantity: order.orderCart[item],
       };
-      array.push(obj);
+      cart.push(obj);
     }
-    setPopOrder(array);
+    order.orderCart = cart;
   })();
 
-  if (popOrder != null) {
+  if (order != null) {
     return (
       <Row justify='center'>
         <Col span={24} className='bookstore-column'>
@@ -37,7 +34,7 @@ function OrderListing(props) {
             <Breadcrumb.Item href='/orderHistory'>
               Order History
             </Breadcrumb.Item>
-            <Breadcrumb.Item href='#'>Order {popOrder.id}</Breadcrumb.Item>
+            <Breadcrumb.Item href='#'>Order {order.id}</Breadcrumb.Item>
           </Breadcrumb>
           <div className='bookstore-page-section'>
             <div className='bookstore-checkout-module-container'>
@@ -45,12 +42,11 @@ function OrderListing(props) {
                 <Title className='bookstore-checkout-module-title'>
                   Shipping Address
                 </Title>
-
                 <Paragraph ellipsis={{ rows: 1 }} style={{ display: 'inline' }}>
-                  <b>{popOrder.address.name}</b> {popOrder.address.address1},{' '}
-                  {popOrder.address.address2} {popOrder.address.city},{' '}
-                  {popOrder.address.state} {popOrder.address.zip}{' '}
-                  {popOrder.address.country}
+                  <b>{order.address.name}</b> {order.address.address1},{' '}
+                  {order.address.address2} {order.address.city},{' '}
+                  {order.address.state} {order.address.zip}{' '}
+                  {order.address.country}
                 </Paragraph>
               </div>
 
@@ -58,17 +54,16 @@ function OrderListing(props) {
                 <Title className='bookstore-checkout-module-title'>
                   Payment Information
                 </Title>
-                {console.log(popOrder)}
                 <Paragraph
                   style={{ marginBottom: '0px' }}
-                  key={popOrder.payment}>
-                  <b>{popOrder.payment.cardType}</b> ending in{' '}
-                  {popOrder.payment.number.slice(-4)}
+                  key={order.payment}>
+                  <b>{order.payment.cardType}</b> ending in{' '}
+                  {order.payment.number.slice(-4)}
                 </Paragraph>
               </div>
 
               <CartList title='Review Cart' />
-              {popOrder.promo ? (
+              {order.promo ? (
                 <div className='bookstore-checkout-module'>
                   <Title className='bookstore-checkout-module-title'>
                     Promotion Information
@@ -76,23 +71,22 @@ function OrderListing(props) {
 
                   <Paragraph
                     style={{ marginBottom: '0px' }}
-                    key={popOrder.promo}>
-                    <b>Promotion code: {popOrder.promo.promoCode}</b> for a %
-                    {popOrder.promo.discount * 100} discount
+                    key={order.promo}>
+                    <b>Promotion code: {order.promo.promoCode}</b> for a %
+                    {order.promo.discount * 100} discount
                   </Paragraph>
                 </div>
               ) : (
                 <div />
               )}
             </div>
-
-            <CartSummary promo={popOrder.promo} action={<div />} />
+            <CartSummary promo={order.promo} action={<div />} order={order}/>
           </div>
         </Col>
       </Row>
     );
   } else {
-    return <div />;
+    return <Spin />;
   }
 }
 
